bbd551137ff93e9755da544e552c72e0260ce8ef
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2022-04-09 11:58:59 +0900

    form component

---
+++ frontend/components/form.tsx
@@ -0,0 +1,56 @@
+import {
+  Badge,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  InputProps,
+} from "@chakra-ui/react";
+import type { FC } from "react";
+
+export type TextFormProps = {
+  label: string;
+  placeholder: string;
+  name: string;
+  isRequired?: boolean;
+} & InputProps;
+
+export const TextForm: FC<TextFormProps> = ({
+  isRequired = false,
+  name,
+  placeholder,
+  label,
+  ...rest
+}) => {
+  return (
+    <FormControl id={name} w="400px">
+      <FormLabel m={1}>
+        <Text display="inline" fontSize="13px" fontWeight="bold">
+          {label}
+        </Text>{" "}
+        {isRequired && (
+          <Badge
+            bg="red.400"
+            color="white"
+            py="3px"
+            px="5px"
+            borderRadius="7px"
+          >
+            必須
+          </Badge>
+        )}
+      </FormLabel>
+
+      <Input
+        placeholder={placeholder}
+        borderColor="gray.500"
+        borderRadius="10px"
+        color="gray.700"
+        _placeholder={{
+          fontSize: "14px",
+        }}
+        {...rest}
+      />
+    </FormControl>
+  );
+};
