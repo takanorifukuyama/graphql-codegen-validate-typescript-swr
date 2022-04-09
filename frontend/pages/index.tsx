4fc286c715defca28afea0e3165b41a55605479e
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2022-04-09 10:41:07 +0900

    resolve zod

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useCallback } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useForm } from "react-hook-form";
+import { UserInput, UserInputSchema } from "../lib/graphql";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Home: NextPage = () => {
+  const { register, handleSubmit } = useForm<UserInput>({
+    resolver: zodResolver(UserInputSchema()),
+  });
+  const onSubmit = useCallback((data: UserInput) => {
+    console.log(data);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -16,41 +26,20 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="email"
+            placeholder="email"
+            {...register("email", { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="name"
+            {...register("message", { required: true, max: 4 })}
+          />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <input type="submit" />
+        </form>
       </main>
 
       <footer className={styles.footer}>
@@ -59,14 +48,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
